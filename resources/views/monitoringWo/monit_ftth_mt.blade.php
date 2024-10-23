bc90b38ef42a49483e24507fd72f2654f796f790
iqbal333 <iqbalgooner33@gmail.com>
2024-10-23 19:56:30 +0700

    fix blade

--- resources/views/monitoringWo/monit_ftth_mt.blade.php
+++ resources/views/monitoringWo/monit_ftth_mt.blade.php
@@ -632,6 +632,7 @@
                                                                 <option value="09:00">09:00</option>
                                                                 <option value="09:30">09:30</option>
                                                                 <option value="10:00">10:00</option>
+                                                                <option value="10:00">10:30</option>
                                                                 <option value="11:00">11:00</option>
                                                                 <option value="11:30">11:30</option>
                                                                 <option value="12:00">12:00</option>
